2847ef3ee80a7b730a307b730c5fc115135e597f
Connor <43049387+Smartiepants109@users.noreply.github.com>
2025-02-03 13:51:54 -0700

    added base version of import function. We'll see how it goes.

---
+++ pipeline/pipe/sp/matimport.py
@@ -0,0 +1,37 @@
+@ -0,0 +1,21 @@
+import substance_painter.resource as res
+CONSTANT_FOR_SMART_MAT = [
+	('rustMetal','G:\skyguard\prototypeAssets\02SmartMaterials\rustMetal.spsm', res.Usage.SMART_MATERIAL),
+	('SG_AssortedMaterials','G:\skyguard\prototypeAssets\02SmartMaterials\SG_AssortedMaterials.spsm', res.Usage.SMART_MATERIAL),
+	('SG_Brass','G:\skyguard\prototypeAssets\02SmartMaterials\SG_Brass.spsm', res.Usage.SMART_MATERIAL),
+	('SG_Copper','G:\skyguard\prototypeAssets\02SmartMaterials\SG_Copper.spsm', res.Usage.SMART_MATERIAL),
+	('SG_GradientMask','G:\skyguard\prototypeAssets\02SmartMaterials\SG_GradientMask.spsm', res.Usage.SMART_MATERIAL),
+	('SG_Metal','G:\skyguard\prototypeAssets\02SmartMaterials\SG_Metal.spsm', res.Usage.SMART_MATERIAL),
+	('SG_MetalBrushed','G:\skyguard\prototypeAssets\02SmartMaterials\SG_MetalBrushed.spsm', res.Usage.SMART_MATERIAL),
+	('SG_MetalPainted','G:\skyguard\prototypeAssets\02SmartMaterials\SG_MetalPainted.spsm', res.Usage.SMART_MATERIAL),
+	('SG_ShipOuterPaint','G:\skyguard\prototypeAssets\02SmartMaterials\SG_ShipOuterPaint.spsm', res.Usage.SMART_MATERIAL),
+	('SM_WoodPlanks','G:\skyguard\prototypeAssets\02SmartMaterials\SM_WoodPlanks.spsm', res.Usage.SMART_MATERIAL), 
+	('SG_Pipe','G:\skyguard\pipeline\gradientMasking\painter_old\SG_Pipe.spsm', res.Usage.SMART_MATERIAL), 
+	('SG_GRM_Template', 'G:\skyguard\prototypeAssets\02SmartMaterials\SP_Export_Presets\SG_GRM_Template.spexp', res.Usage.EXPORT), 
+	('SG_OcRMG_Template', 'G:\skyguard\prototypeAssets\02SmartMaterials\SP_Export_Presets\SG_OcRMG_Template.spexp', res.Usage.EXPORT), 
+	('SG_OpRMG_Template', 'G:\skyguard\prototypeAssets\02SmartMaterials\SP_Export_Presets\SG_OpRMG_Template.spexp', res.Usage.EXPORT)
+]
+def isInstalled(material):
+	pot = res.search(material[0])
+	if(len(pot)== 1):
+		return True
+	elif (len(pot) == 0)
+		return False
+	elif (len(pot) >1):
+		print("Potential Error occured when installing:",material, "what the heck: More than one installed? Check naming conventions! These should be unique names!!!!!")
+		return True
+
+def install(installConst):
+	import_project_resource(installConst[1], installConst[2], installConst[0], None)
+
+
+
+def matImport():
+    for mat in CONSTANT_FOR_SMART_MAT:
+	    if not isInstalled(mat):
+		    install(mat)
